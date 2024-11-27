0d3063b3c86e9b3138edfd7aeec80d21119f7c4a
dio <bagusdio01@gmail.com>
2024-11-27 16:02:43 +0800

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
+import React, { useEffect } from 'react';
 import "@/styles/globals.css";
+import Layout from "@/components/layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css";
+    link.rel = "stylesheet";
+    document.head.appendChild(link);
+  }, []);
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
